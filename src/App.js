37fc709ea044dd9596ff66377fa838c0045a9436
deployer420 <117289105+deployer420@users.noreply.github.com>
2022-12-22 22:50:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -196,7 +196,7 @@ function App() {
 
   useEffect(() => {
     // Calculate the time remaining in the timer
-    const endTime = new Date("2022-12-21T22:00:00Z");
+    const endTime = new Date("2022-12-21T23:00:00Z");
     const timeRemaining = endTime - new Date();
 
     // Set the initial time remaining
@@ -278,7 +278,7 @@ function App() {
             <s.SpacerLarge />
 
             <s.TextTitle>
-              Public mint time: 6 PM EST 22/12/2023
+              Public mint time: 7 PM EST 22/12/2023
             </s.TextTitle>
 
             <s.TextTitle>
